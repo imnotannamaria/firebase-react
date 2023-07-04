29c506b5fbd44473a13817e85d8f3f9fdeab010c
Anna Maria <aannamariabr@outlook.com>
2023-07-04 19:43:20 -0300

    Removing comments

--- src/App.tsx
+++ src/App.tsx
@@ -63,24 +63,5 @@ export function App() {
         </Button>
       </form>
     </Box>
-
-    // <div>
-    //   <h2>Login</h2>
-    //   <form onSubmit={handleLogin}>
-    //     <input
-    //       type="email"
-    //       placeholder="Email"
-    //       value={email}
-    //       onChange={(e) => setEmail(e.target.value)}
-    //     />
-    //     <input
-    //       type="password"
-    //       placeholder="Senha"
-    //       value={password}
-    //       onChange={(e) => setPassword(e.target.value)}
-    //     />
-    //     <button type="submit">Entrar</button>
-    //   </form>
-    // </div>
   )
 }
